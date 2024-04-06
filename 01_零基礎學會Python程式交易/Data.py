52ea817e0b4f48275c4861feb437ed6c2808d429
samhsiao6238 <samhsiao6238@gmail.com>
2024-04-06 09:37:45 +0800

    更新

--- 01_零基礎學會Python程式交易/Data.py
+++ 01_零基礎學會Python程式交易/Data.py
@@ -2,14 +2,7 @@
 import os
 import yfinance as yf
 import pandas as pd
-from FinMind.data import DataLoader
 
-api = DataLoader()
-
-# 資料存取路徑(如果路徑不存在 則建立一個目錄)
-dpath = "data"
-if not os.path.exists(dpath):
-    os.makedirs(dpath)
 
 # yahoo 取用資料函數
 
@@ -38,38 +31,7 @@ def getDataFM(prod):
     return yfdata
 
 
-# finmind 取用資料函數
-"""
-
-def getDataFM(prod):
-    # 1. 定義備份檔案名稱
-    bakfile = "%s\\fm_%s.csv" % (dpath, prod)
-    # 2. 檢視是否有歷史資料(本地端)
-    if os.path.exists(bakfile):
-        fmdata = pd.read_csv(bakfile)
-        fmdata["date"] = pd.to_datetime(fmdata["date"], format="%Y-%m-%d")
-        fmdata.set_index("date", inplace=True)
-    else:
-        fmdata = api.taiwan_stock_daily_adj(
-            stock_id=prod, start_date="1900-01-01", end_date="2100-01-01"
-        )
-        # 設定時間索引
-        fmdata["date"] = pd.to_datetime(fmdata["date"], format="%Y-%m-%d")
-        fmdata.set_index("date", inplace=True)
-        # 欄位改名
-        fmdata.rename(
-            columns={"max": "high", "min": "low", "Trading_Volume": "volume"},
-            inplace=True,
-        )
-        # finmind 存檔
-        bakfile = "%s\\fm_%s.csv" % (dpath, prod)
-        fmdata.to_csv(bakfile)
-    return fmdata
-"""
-
 # yahoo 取用資料函數
-
-
 def getDataYF(prod):
     # 備份文件的資料夾
     folder_name = "data"
