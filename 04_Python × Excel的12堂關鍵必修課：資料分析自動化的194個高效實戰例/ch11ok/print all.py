b9178d8fe217e9839fbe2588823d604c434dd299
samhsiao6238 <samhsiao6238@gmail.com>
2024-04-10 10:13:56 +0800

    更新

--- 04_Python × Excel的12堂關鍵必修課：資料分析自動化的194個高效實戰例/ch11ok/print all.py
+++
@@ -1,6 +0,0 @@
-import xlwings as xw
-app = xw.App(visible=False, add_book=False)
-wb = app.books.open('指考成績.xlsx')
-wb.api.PrintOut(Copies=1, ActivePrinter='Microsoft Print to PDF', Collate=True)
-wb.close()
-app.quit()
